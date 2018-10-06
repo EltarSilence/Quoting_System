d26203f0c785cbec0271c93d9b73c06ac831e454
iamousseni <ousseni.work@gmail.com>
2018-10-06 10:30:58 +0200

    login 20181006

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@ session_start();
 if( isset($_SESSION['user_id']) ){
 	header("Location: /");
 }
-
+$message="";
 if(!empty($_POST['username']) && !empty($_POST['password'])){
 	$conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
 	$sql = "SELECT * FROM utenti WHERE username = '".$_POST['username']."' AND password = '".md5($_POST['password'])."'";
@@ -29,29 +29,44 @@ if(!empty($_POST['username']) && !empty($_POST['password'])){
 }
 ?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Login Below</title>
+<html lang="en">
+<head>
+    <title>Login Below</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link rel="stylesheet" href="../icon.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="script.js"></script>
-	</head>
-	<body>
-		<div class="header">
-			Quoting by Rizza, Ravasi, Gallina
-		</div>
-		<?php 
-			if(!empty($message)){
-				echo "<p>".$message."</p>";
-			}
-		?>
-		<h1 class="white">Login</h1><span class="white">or <a href="register.php">register here</a></span>
-		<form action="login.php" method="POST">
-			<input type="text" placeholder="Username" name="username">
-			<input type="password" placeholder="Password" name="password">
-			<input type="submit">
-		</form>
-	</body>
+
+</head>
+<body class="siign">
+        <div class="sign-container">
+            <div>
+                <div class="left-bk">
+                </div>
+                <div>
+                    <h1>Sign-in</h1>
+                    <h3>Join with Us and Have Fun!</h3>
+                </div>
+            </div>
+            <div class="right-frm">
+                <div>
+                    <h1 <?php if(!empty($message) || $message==""){echo "style='margin-bottom:0'";} ?>>Sign-in</h1>
+					<?php 
+						if(!empty($message)){
+							echo "<p class='message'>".$message."</p>";
+						}
+					?>
+                    <form action="login.php" method="POST">
+                        <input type="text" placeholder="Username" name="username">
+                        <input type="password" placeholder="Password" name="password">
+                        <input type="submit" value="INVIA">
+                    </form>
+                </div>
+            </div>
+        </div>
+</body>
 </html>
