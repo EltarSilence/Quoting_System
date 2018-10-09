1abc0559a60be169cf288abec7dc21c61b66fc76
Zexal0807 <gallinar00@gmail.com>
2018-10-09 12:04:02 +0200

    Fix - anche se non attivato potevi loggarti

--- login/login.php
+++ login/login.php
@@ -7,7 +7,7 @@ if( isset($_SESSION['user_id']) ){
 $message="";
 if(!empty($_POST['username']) && !empty($_POST['password'])){
 	$conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
-	$sql = "SELECT * FROM utenti WHERE username = '".$_POST['username']."' AND password = '".md5($_POST['password'])."'";
+	$sql = "SELECT * FROM utenti WHERE username = '".$_POST['username']."' AND password = '".md5($_POST['password'])."' AND status = 1";
 	$result = mysqli_query($conn, $sql);
 	if(mysqli_num_rows($result) == 1){
 		$row = mysqli_fetch_assoc($result);
@@ -15,7 +15,7 @@ if(!empty($_POST['username']) && !empty($_POST['password'])){
 		mysqli_close($conn);
 		header("Location: /");
 	}else{
-		$sql = "SELECT * FROM utenti WHERE email = '".$_POST['username']."' AND password = '".md5($_POST['password'])."'";
+		$sql = "SELECT * FROM utenti WHERE email = '".$_POST['username']."' AND password = '".md5($_POST['password'])."' AND status = 1";
 		$result = mysqli_query($conn, $sql);
 		if(mysqli_num_rows($result) == 1){
 			$row = mysqli_fetch_assoc($result);
