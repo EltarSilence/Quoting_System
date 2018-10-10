b2971776c7c3ee0a043c8ecfa78eb637e926f85f
Zexal0807 <gallinar00@gmail.com>
2018-10-10 11:43:16 +0200

    Miglioramento del codice

--- generazioneQuote/writeExcelData.php
+++ generazioneQuote/writeExcelData.php
@@ -1,9 +1,9 @@
 <?php
-require_once '../PHPExcel/Classes/PHPExcel.php';
-require_once('../config.php');
-require_once('../functions.php');
+	require_once('../PHPExcel/Classes/PHPExcel.php');
+	require_once('../config.php');
+	require_once('../functions.php');
 
-//PROVA di esecuzione
-newScommessa($_POST["data"], $_POST["materia"]);
+	//PROVA di esecuzione
+	newScommessa($_POST["data"], $_POST["materia"]);
 
 ?>
