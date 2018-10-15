c26964f36757dd6b2fc6da55a576c38c342e7688
Giovanni Rizza <rizza67jorge@gmail.com>
2018-10-15 09:30:06 +0200

    Aggiunta biglietto

---
+++ biglietto/biglietto.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="../icon.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<style>
+	.row {
+		margin: 0;
+	}
+
+	.top {
+		color: white;
+		font-weight: bold;
+		background-color: black;
+	}
+</style>
+<body>
+	<div class="row top">
+		Biglietto
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-xs-12"><b>Verifica di Matematica</b></div>
+	</div>
+	<div class="row">
+		<div class="col-xs-8"><i>RIZZA G: VOTO ESATTO 4</i> - </div>
+		<div class="col-xs-4"><b>4,61</b> <i class="icon icon-exacoin"></i></div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-xs-12"><b>Verifica di Informatica</b></div>
+	</div>
+	<div class="row">
+		<div class="col-xs-8"><i>MARCHI A: OVER 6.75</i> - </div>
+		<div class="col-xs-4"><b>1,52</b> <i class="icon icon-exacoin"></i></div>
+	</div>
+	<hr>
+
+	<button>Scommetti</button>>
+</body>
+</html>
